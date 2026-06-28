69dca099e4ea9d5f0c40e4bc485d9365d9b424bf
Yashvanth D <vtu29230@veltech.edu.in>
2026-06-28 14:26:29 +0530

    update code

---
+++ websitemonitore/src/main/java/com/yash/websitemonitore/WebsitemonitoreApplication.java
@@ -0,0 +1,13 @@
+package com.yash.websitemonitore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitemonitoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsitemonitoreApplication.class, args);
+	}
+
+}
---
+++ websitemonitore/src/test/java/com/yash/websitemonitore/WebsitemonitoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.websitemonitore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitemonitoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
